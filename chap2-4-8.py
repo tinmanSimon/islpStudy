f49a9ddf52dc0dabbaadb4408bcf80dfdeba2466
Simon <wudisas@gmail.com>
2024-05-04 22:15:41 -0400

    2.4.8 finished

--- chap2-4-8.py
+++ chap2-4-8.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # 8.a
 print("Question 8.a:")
@@ -42,11 +43,23 @@ college.boxplot(column='Outstate', by='Elite')
 plt.suptitle('8.f Outstate/Elite Boxplot')
 print("*******************************\n\n\n")
 
+# 8.g
+print("Question 8.g:")
+fig, ax = plt.subplots(nrows=2, ncols=2)
+ax[0,0].title.set_text("Grad.Rate")
+college.plot.hist(density=True, column='Grad.Rate', ax=ax[0,0], edgecolor='White', bins=np.arange(0, max(college['Grad.Rate']) + 1, 10))
+ax[0,1].title.set_text("Enroll")
+college.plot.hist(density=True, column='Enroll', ax=ax[0,1], edgecolor='White', bins=np.arange(0, max(college['Enroll']) + 1, 500))
+ax[1,0].title.set_text("S.F.Ratio")
+college.plot.hist(density=True, column='S.F.Ratio', ax=ax[1,0], edgecolor='White', bins=np.arange(0, max(college['S.F.Ratio']) + 1, 10))
+ax[1,1].title.set_text("Top10perc")
+college.plot.hist(density=True, column='Top10perc', ax=ax[1,1], edgecolor='White', bins=np.arange(0, max(college['Top10perc']) + 1, 10))
+print("*******************************\n\n\n")
 
-
-
-
-
+# 8.h
+print("Question 8.g:")
+print("The data of Grad.Rate is wrong. We have values like 118, how can you graduate at 118%? doesn't make sense.")
+print("*******************************\n\n\n")
 
 
 
